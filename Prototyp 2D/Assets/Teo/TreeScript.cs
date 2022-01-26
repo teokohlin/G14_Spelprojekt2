0826969f6fe540d3eb3ece7124d3a57e7e793d3e
teokohlin <teo@kohlin.net>
2022-01-26 15:31:58 +0100

    Träd system(typ)

---
+++ Prototyp 2D/Assets/Teo/TreeScript.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeScript : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform health;
+    public GameObject TreeHealth;
+    public GameObject Buttons;
+    private float originalSize;
+    private bool clickedyes = false;
+    void Start()
+    {
+        Time.timeScale = 1f;
+        originalSize = health.sizeDelta.x;
+    }
+
+    void Update()
+    {
+        if (clickedyes)
+        {
+            if (health.sizeDelta.x > 0)
+            {
+                health.sizeDelta += new Vector2(-0.01f/4, 0);
+            }
+            else
+            {
+                TreeHealth.SetActive(false);
+                health.sizeDelta = new Vector2(originalSize, health.sizeDelta.y);
+                clickedyes = false;
+            }
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Buttons.SetActive(true);
+            Debug.Log("enter");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Buttons.gameObject.SetActive(false);
+            Debug.Log("exit");
+
+        }
+    }
+
+    public void Yes()
+    {
+        Buttons.SetActive(false);
+        TreeHealth.SetActive(true);
+        clickedyes = true;
+    }
+
+    public void No()
+    {
+        Buttons.SetActive(false);
+    }
+}
